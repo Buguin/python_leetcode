302b4bb3a2313794580cfcaad2130d15374c3059
buguin <wybuguin@hotmail.com>
2020-11-28 00:28:07 +0800

    454. 四数相加 II

--- common11/SolutionSildeWindow.py
+++ common11/SolutionSildeWindow.py
@@ -32,24 +32,3 @@ class Solution:
             que.push(nums[i])
             m_ans.append(que.front())
         return m_ans
-
-    @staticmethod
-    def fourSumCount( A:[int], B: [int], C: [int], D: [int]) -> int:
-        sum_a_b = {}
-        for a in A:
-            for b in B:
-                temp_sum = a + b
-                sum_a_b[temp_sum] = sum_a_b.get(temp_sum, 0) + 1
-                # if temp_sum not in sum_a_b.keys():
-                #     sum_a_b[temp_sum] = 1
-                # else:
-                #     sum_a_b[temp_sum] += 1
-        ans = 0
-        for c in C:
-            for d in D:
-                temp_sum = c + d
-                if -temp_sum in sum_a_b:
-                    ans += sum_a_b[-temp_sum]
-                # if -temp_sum in sum_a_b.keys():
-                #     ans += sum_a_b[-temp_sum]
-        return ans
