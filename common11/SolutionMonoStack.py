5cc7e1199f2cb0f14d05941016946e1101336c6e
buguin <wybuguin@hotmail.com>
2020-12-18 22:31:56 +0800

    714. 买卖股票的最佳时机含手续费

--- common11/SolutionMonoStack.py
+++ common11/SolutionMonoStack.py
@@ -21,3 +21,12 @@ class Solution:
                 ans += 1
                 pre_diff = cur_diff
         return ans
+
+    @staticmethod
+    def maxProfit(prices: [int], fee: int) -> int:
+        n = len(prices)
+        dp = [[0, -prices[0]]] + [[0, 0] for _ in range(n - 1)]
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+        return dp[n - 1][0]
