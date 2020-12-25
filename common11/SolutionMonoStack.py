4d1b385644694ef1551933eeddda1e1544d411b9
buguin <wybuguin@hotmail.com>
2020-12-26 01:28:21 +0800

    85. 最大矩形

--- common11/SolutionMonoStack.py
+++ common11/SolutionMonoStack.py
@@ -21,3 +21,30 @@ class Solution:
                 ans += 1
                 pre_diff = cur_diff
         return ans
+
+    @staticmethod
+    def maximalRectangle(matrix: [[str]]) -> int:
+        size_r = len(matrix)
+        if not size_r: return 0
+        size_c = len(matrix[0])
+        ans = 0
+        def maximalList(list_int: [int]) -> int:
+            ans_l = 0
+            mono_stack = []
+            list_int = [-1] + list_int + [-1]
+            for i in range(len(list_int)):
+                while mono_stack and list_int[mono_stack[-1]] > list_int[i]:
+                    hight = list_int[mono_stack.pop(-1)]
+                    wight = i - mono_stack[-1] - 1
+                    ans_l = max(ans_l, hight * wight)
+                mono_stack.append(i)
+            return ans_l
+        matrix_list = [0] * size_c
+        for i in range(size_r):
+            for j in range(size_c):
+                if int(matrix[i][j]):
+                    matrix_list[j] += int(matrix[i][j])
+                else:
+                    matrix_list[j] = 0
+            ans = max(ans, maximalList(matrix_list))
+        return ans
--- test/main_test_mono_stack.py
+++ test/main_test_mono_stack.py
@@ -18,6 +18,22 @@ class MyTestCase(unittest.TestCase):
         else:
             self.assertEqual(True, True)
 
+    def test_maximalRectangle(self):
+        """
+        85. 最大矩形
+        """
+        # nums = [1,7,4,9,2,5]
+        matrix = [["1", "0", "1", "0", "0"],
+                  ["1", "0", "1", "1", "1"],
+                  ["1", "1", "1", "1", "1"],
+                  ["1", "0", "0", "1", "0"]]
+        ans = Solution.maximalRectangle(matrix)
+        print(ans)
+        if ans == 0:
+            self.assertEqual(True, False)
+        else:
+            self.assertEqual(True, True)
+
 
 if __name__ == '__main__':
     unittest.main()
